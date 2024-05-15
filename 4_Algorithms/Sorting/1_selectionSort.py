5ba8863f0c1a81073a22ed012daa764501d97d48
hsmgowtham <hsmgowtham@gmail.com>
2024-05-15 02:56:51 -0400

    Implement Selection Sort

---
+++ 4_Algorithms/Sorting/1_selectionSort.py
@@ -0,0 +1,21 @@
+"""
+Selection Sort:
+----------------
+- Works by repeatedly finding the minimum element from the unsorted portion and moving it to the beginning.
+- Time complexity: O(n^2) in all cases.
+- Not suitable for large datasets due to its quadratic time complexity.
+- In-place sorting algorithm.
+"""
+
+def selection_sort(arr):
+    for i in range(0, len(arr)-1): 
+        min_ele_position = i       
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_ele_position]:
+                min_ele_position = j
+        print(i, j, arr[i], arr[min_ele_position])
+        arr[i],arr[min_ele_position] = arr[min_ele_position],arr[i]
+    return arr
+
+arr = [5,4,3,2,1]
+print(selection_sort(arr))
