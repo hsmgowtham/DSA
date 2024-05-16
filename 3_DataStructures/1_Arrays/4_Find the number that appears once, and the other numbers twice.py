a040459357c1fbaca6cdffcb2e2e74f5c571a3ce
hsmgowtham <hsmgowtham@gmail.com>
2024-05-16 05:16:13 -0400

    Update Arrays

---
+++ 3_DataStructures/1_Arrays/4_Find the number that appears once, and the other numbers twice.py
@@ -0,0 +1,47 @@
+"""
+Find the number that appears once, and the other numbers twice
+
+Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
+Input Format:
+ arr[] = {2,2,1}
+Result:
+ 1
+Explanation:
+ In this array, only the element 1 appear once and so it is the answer.
+
+Example 2:
+Input Format:
+ arr[] = {4,1,2,1,2}
+Result:
+4
+
+Approach:
+Two important properties of XOR are the following:
+
+XOR of two same numbers is always 0 i.e. a ^ a = 0. ←Property 1.
+XOR of a number with 0 will result in the number itself i.e. 0 ^ a = a.  ←Property 2
+
+Here all the numbers except the single number appear twice and so will form a pair. Now, if we perform XOR of all elements of the array, the XOR of each pair will result in 0 according to the XOR property 1. The result will be = 0 ^ (single number) = single number (according to property 2).
+
+So, if we perform the XOR of all the numbers of the array elements, we will be left with a single number.
+
+Approach:
+We will just perform the XOR of all elements of the array using a loop and the final XOR will be the answer.
+Dry run:
+
+Assume the given array is: [4,1,2,1,2]
+XOR of all elements = 4^1^2^1^2
+      = 4 ^ (1^1) ^ (2^2)
+      = 4 ^ 0 ^ 0 = 4^0 = 4
+Hence, 4 is the single element in the array.
+"""
+
+def singleNumber(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    xorr = 0
+    for num in nums:
+        xorr ^= num
+    return xorr
