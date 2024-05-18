a2297fd635fc98bb8a716565e5d3467f94928531
hsmgowtham <hsmgowtham@gmail.com>
2024-05-18 08:38:30 -0400

    Update LL Operations

---
+++ 3_DataStructures/2_LinkedLists/1_Operations.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtbeg(self, data):
+        new_node = Node(data) 
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+    
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+        return
+
+    def printLL(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+
+
+ll = LinkedList()
+ll.insertAtbeg(5)
+ll.insertAtbeg(6)
+ll.insertAtEnd(8)
+ll.printLL()
