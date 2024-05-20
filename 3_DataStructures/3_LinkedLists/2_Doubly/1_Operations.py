d2c9065802141cb7a548cd137a01536feb748cec
hsmgowtham <hsmgowtham@gmail.com>
2024-05-20 08:07:55 -0400

    Update Doubly LL Operations

--- 3_DataStructures/3_LinkedLists/2_Doubly/1_Operations.py
+++ 3_DataStructures/3_LinkedLists/2_Doubly/1_Operations.py
@@ -4,10 +4,11 @@ class Node:
         self.data = data
         self.next = None
 
+
 class DoublyLL:
     def __init__(self):
         self.head = None
-    
+
     def insertAtBeg(self, data):
         new_node = Node(data=data)
         if not self.head:
@@ -17,7 +18,7 @@ class DoublyLL:
         self.head.pre = new_node
         self.head = new_node
         return
-    
+
     def insertAtEnd(self, data):
         new_node = Node(data=data)
         current_node = self.head
@@ -29,7 +30,7 @@ class DoublyLL:
         current_node.next = new_node
         new_node.pre = current_node
         return
-    
+
     def insertAtPos(self, data, pos):
         new_node = Node(data)
         current_node = self.head
@@ -44,14 +45,26 @@ class DoublyLL:
         new_node.next = current_node
         current_node.pre = new_node
         return
-            
-    
+
+    def search(self, data):
+        pos = 1
+        current_node = self.head
+        while current_node.data != data:
+            current_node = current_node.next
+            pos += 1
+        else:
+            if current_node.data == data:
+                print(f"Found at Index {pos}")
+            else:
+                print("Given Data Not Found")
+
     def printLL(self):
         current_node = self.head
         while current_node:
             print(current_node.data, end=" -> ")
             current_node = current_node.next
         print()
+
 
 ll = DoublyLL()
 print("--- Insert At Beg 5 ---")
@@ -69,3 +82,7 @@ ll.printLL()
 print("--- Insert At Pos 2 = 9 ---")
 ll.insertAtPos(9, 2)
 ll.printLL()
+print("--- Search 6 ---")
+ll.search(6)
+print("--- Search 8 ---")
+ll.search(8)
