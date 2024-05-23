ffc8d96ed028fc4d63a192d60f3024718bf322ab
hsmgowtham <hsmgowtham@gmail.com>
2024-05-23 01:51:06 -0400

    Update Strings

---
+++ 3_DataStructures/2_Strings/3_two_str_are_anagram.py
@@ -0,0 +1,30 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+"""
+def isAnagram(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    if len(s) != len(t):
+        return False
+    for idx in set(s):
+        # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
+        # If they are different, return false...
+        if s.count(idx) != t.count(idx):
+            return False
+    return True
