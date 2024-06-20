46f6dfcc9be24be68413e522247a069c83a3c593
hsmgowtham <hsmgowtham@gmail.com>
2024-06-20 06:11:52 -0400

    Update Next Greater Element using Circular arr

--- 3_DataStructures/4_Stacks/4_1_Monotonic Stack/2_Next Greater Element Using Stack2.py
+++ 3_DataStructures/4_Stacks/4_1_Monotonic Stack/2_Next Greater Element Using Stack2.py
@@ -1,29 +1,39 @@
 """
-Next Greater Element in a Circullar Array
+Next Greater Element - Circular Array
+Input: N = 11, A[] = {2, 10, 12, 1, 11}
+
+Output: 10, 12, -1, 11, 12
+
+
+Algorithm:
+- traverse from right to left (2*size) => {2, 10, 12, 1, 11, 2, 10, 12, 1, 11}
+                                          {0, 1,   2, 3, 4,  5,  6,  7, 8, 9}
+- if elements in stack and remove elements that are less than ele in the arr to find the nearest greater ele 
+    - now the top will be nge of current ele
+    - add ele to nge only if i < size
+- append current ele to stack
 """
-from typing import List
 
 
-class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        nge = [-1] * n
-        st = []
+def nextGreaterElement(arr):
+    size = len(arr)
+    nge_arr = [-1] * size
+    st = []
 
-        for i in range(2 * n - 1, -1, -1):
-            while st and st[-1] <= nums[i % n]:
-                st.pop()
+    for i in range(2 * size - 1, -1, -1):
+        while st and st[-1] <= arr[i % size]:
+            st.pop()
 
-            if i < n:
-                if st:
-                    nge[i] = st[-1]
-            st.append(nums[i % n])
-        return nge
+        if i < size:
+            if st:
+                nge_arr[i] = st[-1]
+        st.append(arr[i % size])
+    return nge_arr
 
 
 if __name__ == "__main__":
-    obj = Solution()
-    v = [5, 7, 1, 2, 6, 0]
-    res = obj.nextGreaterElements(v)
+    arr = [3, 10, 4, 2, 1, 2, 6, 1, 7, 2, 9]
+    res = nextGreaterElement(arr)
     print("The next greater elements are")
+    print(*arr)
     print(*res)
