6d2338481be7f8e15423eba6cba4476d7ee70865
hsmgowtham <hsmgowtham@gmail.com>
2024-06-20 05:53:46 -0400

    Update NGE without Circular arr

--- 3_DataStructures/4_Stacks/4_1_Monotonic Stack/1_Next Greater Element Using Stack.py
+++ 3_DataStructures/4_Stacks/4_1_Monotonic Stack/1_Next Greater Element Using Stack.py
@@ -1,26 +1,35 @@
-from typing import List
+"""
+Next Greater Element - Fixed Array(Not Circular)
+Input: N = 11, A[] = {3,10,4,2,1,2,6,1,7,2,9}
+
+Output: 10,-1,6,6,2,6,7,7,9,9,-1
 
 
-class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        nge = [-1] * n
-        st = []
+Algorithm:
+- traverse from right to left
+- if elements in stack and remove elements that are less than ele in the arr to find the nearest greater ele 
+    - now the top will be nge of current ele
+- append current ele to stack
+"""
+def nextGreaterElement(arr):
+    size = len(arr)
+    nge_arr = [-1] * size
+    st = []
 
-        for i in range(n - 1, -1, -1):
-            while st and st[-1] <= nums[i % n]:
-                st.pop()
-
-            if i < n:
-                if st:
-                    nge[i] = st[-1]
-            st.append(nums[i % n])
-        return nge
-
+    for i in range(size-1, -1, -1):
+        # while st and st[-1]<= arr[i]:
+        #     # st.pop()
+        #     pass
+        if st:
+            nge_arr[i] = st[-1]
+        st.append(arr[i])
+    return nge_arr
+        
+        
 
 if __name__ == "__main__":
-    obj = Solution()
-    v = [5, 7, 1, 2, 6, 0]
-    res = obj.nextGreaterElements(v)
+    arr = [3,10,4,2,1,2,6,1,7,2,9]
+    res = nextGreaterElement(arr)
     print("The next greater elements are")
+    print(*arr)
     print(*res)
