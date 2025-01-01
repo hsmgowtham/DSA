23f2f1237046496faa5268f5fb2a525947b81165
hsmgowtham <hsmgowtham@gmail.com>
2024-12-31 22:04:15 -0500

    Update move all zeroes to right

--- 3_DataStructures/1_Arrays/1_Move all Zeros to the end of the array.py
+++ 3_DataStructures/1_Arrays/1_Move all Zeros to the end of the array.py
@@ -4,7 +4,29 @@ Input:
 Output:
  1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
 """
-
+"""
+Brute Force Approach
+Time Complexity: O(k*(n-k)) ~ O(N^2) if all the elements are zeroes
+"""
+def moveZeroes2(nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 1
+        swap_start_index = 0
+        while i < len(nums):
+            if nums[i]!=0 and nums[i-1]==0:
+                swap_start_index = i
+                while nums[i-1]==0 and i-1>=0:
+                    nums[i], nums[i-1] = nums[i-1], nums[i]
+                    i -= 1
+                i = swap_start_index + 1
+            else:
+                i += 1
+"""
+Optimal Solution:
+Time Complexity: O(N)
+"""
 def moveZeroes(self, nums):
     """
     :type nums: List[int]
